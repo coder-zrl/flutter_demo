4ee7edc16292e4789e7abbf51fc668497b9ecc9c
coder-zrl <coder-zrl@qq.com>
2025-08-06 01:29:16 +0800

    feat: BottomNavigationBar 底部导航栏

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,14 @@
 // importM
 import 'package:flutter/material.dart';
 
-import 'date/flutter09_statefulWidget.dart';
+import './pages/tabs.dart';
 
 void main() {
   runApp(
     // 所有的Flutter应用都需要MaterialApp包裹
     MaterialApp(
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: MyApp()
+      home: Tabs()
     ),
   );
 }
---
+++ lib/pages/tabs/category.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class CategoryPage extends StatefulWidget {
+  const CategoryPage({super.key});
+
+  @override
+  State<CategoryPage> createState() => _CategoryPageState();
+}
+
+class _CategoryPageState extends State<CategoryPage> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        ListTile(title: Text('我是一个列表')),
+        ListTile(title: Text('我是一个列表')),
+        ListTile(title: Text('我是一个列表')),
+        ListTile(title: Text('我是一个列表')),
+        ListTile(title: Text('我是一个列表')),
+        ListTile(title: Text('我是一个列表')),
+      ],
+    );
+  }
+}
